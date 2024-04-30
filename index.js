fec30b3da676875d7ee9bd48a876ada426b6f3bf
j0yglvdy5 <joygladys.njeri@student.moringaschool.com>
2024-04-30 15:00:01 +0300

    phase 1

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // I'm suppossed to create a Write a function isPalindrome that will receive one argument, a string.
+  // Your function should return true if the string is a palindrome (that is, if it reads the same forwards and backwards, like "mom" or "racecar"), and return false if it is not a palindrome.
+  //To keep things simple, your function only needs to deal with lowercase strings that are all letters (don't worry about spaces or special characters).
+  word.toLocaleLowerCase();
+  let newWord = word.split("").reverse().join("");
+  if (word === newWord) {
+    return true;
+} else {
+    return false;
+}
+
 }
 
 /* 
   Add your pseudocode here
+  convert to lowercase 
+  split -> "abba"=== "a""b""b""a"
+  reverse -> "a""b""b""a"=== "a""b""b""a"
+  join -> "a""b""b""a"=== "abba"
 */
 
 /*
@@ -13,10 +27,10 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  //console.log("Expecting: true");
+ // console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+ console.log("");
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
